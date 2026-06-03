20161ace4491172527e29f1cdb3e95cfb69d4b15
Rishav Raj <rishav.24b1541080@abes.ac.in>
2026-06-03 21:39:51 +0530

    handle form

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,13 @@
+import { useState } from "react";
+
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "mobile", quantity: 1, packed: true },
+];
+
+
 export default function App() {
   return <div className="app">
     <Logo />
@@ -18,24 +28,63 @@ function Logo() {
 
 
 function Form() {
+
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    // reset form after submit
+    setDescription("")
+    setQuantity(1)
+
+  }
+
   return (
     <div>
-      <div className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for you trip</h3>
-      </div>
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+          {/* automatically generate 20  list numbers */}
+          {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+        </select>
+        <input type="text" placeholder="item..." value={description} onChange={(e) => setDescription(e.target.value)} />
+        {/* This call back function in onchange is used to read the value which is entered in the  from */}
+        <button>Add</button>
+      </form>
 
     </div>
   )
 
 }
 
-
 function PackingList() {
   return (
     <div className="list">
-      LIST
+
+      <ul >
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
     </div>
-  )
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 
