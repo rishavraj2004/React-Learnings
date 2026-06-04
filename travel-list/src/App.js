2d7f76fe2f043eba0122719f622d0f284a7993f6
Rishav Raj <rishav.24b1541080@abes.ac.in>
2026-06-04 13:39:49 +0530

    Sorting Feature

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -89,14 +89,37 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onTogggleItems }) {
+
+  // sort
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === 'input') sortedItems = items;
+
+  if (sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === 'packed') sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+
+
   return (
     <div className="list">
 
       <ul >
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item key={item.id} onDeleteItem={onDeleteItem} item={item} onTogggleItems={onTogggleItems} />
         ))}
       </ul>
+
+      <div>
+        <select className="actions" onChange={e => setSortBy(e.target.value)}>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description order</option>
+          <option value='packed'>Sort by packed order</option>
+        </select>
+      </div>
+
+
+
     </div>
   );
 }
