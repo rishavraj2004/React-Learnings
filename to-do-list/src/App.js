363ff4d881ca73f72e647ecabc808002e9bd1ec1
Rishav Raj <rishav.24b1541080@abes.ac.in>
2026-06-06 10:40:00 +0530

    Added all features

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
 export default function App() {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems(items => [...items, item])
+    }
+    function handleDelteItems(id) {
+        setItems(items => items.filter(item => item.id !== id));
+    }
+    function handleToggleItems(id) {
+        setItems(items => items.map(item => item.id === id ? { ...item, completed: !item.completed } : item))
+    }
+    function handleClearList() {
+        setItems([]);
+    }
+
+
     return (
         <div className="App">
             <div className="container">
 
                 <Logo />
-                <Form />
-                <Item />
+                <Form onAddItems={handleAddItems} />
+                <TaskList items={items} onDeleteItem={handleDelteItems} onToggleItems={handleToggleItems} onClearItems={handleClearList} />
                 <Stats />
             </div>
         </div>
@@ -22,17 +40,28 @@ function Logo() {
     )
 }
 
-function Form() {
+function Form({ onAddItems }) {
+
+    const [description, setDescription] = useState("");
+
+
+
     function handleSubmit(e) {
         e.preventDefault();
+
+        const newItem = { description, completed: false, id: Date.now() }
+
+        if (!description) return;
+        onAddItems(newItem)
+        setDescription("")
     }
     return (
         <div className="form">
-            <form className="add-form" onSubmit={{ handleSubmit }}>
+            <form className="add-form" onSubmit={handleSubmit}>
                 {/* <h3>
                     Add and manage you task!😊
                 </h3> */}
-                <input className="input-task" type="text" placeholder="Add your task..." />
+                <input className="input-task" type="text" placeholder="Add your task..." value={description} onChange={(e) => setDescription(e.target.value)} />
                 <button className="input-button">Add</button>
             </form>
 
@@ -40,16 +69,44 @@ function Form() {
     )
 }
 
-function Item() {
+
+
+
+
+function Item({ item, onDeleteItem, onToggleItems }) {
     return (
         <li>
-            <input type="checkbox" />
-            <span>item</span>
-            <button>Delete</button>
+            <input type="checkbox" checked={item.completed} onChange={() => onToggleItems(item.id)} />
+            <span style={item.completed ? { textDecoration: "line-through" } : {}}>
+                {item.description}
+            </span>
+            <button onClick={() => onDeleteItem(item.id)}>
+                Delete
+            </button>
         </li>
     )
 
 }
+
+
+function TaskList({ items, onDeleteItem, onToggleItems, onClearItems }) {
+    return (
+        <div>
+            <ul>
+                {items.map((item) => (
+                    <Item
+                        key={item.id}
+                        item={item}
+                        onDeleteItem={onDeleteItem}
+                        onToggleItems={onToggleItems}
+                    />
+                ))}
+            </ul>
+        </div>
+    )
+
+}
+
 
 function Stats() {
     return <footer className="stats">
