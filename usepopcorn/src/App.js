0327641856d4151c0d7c3ddd5a28d4985892ee32
Rishav Raj <rishav.24b1541080@abes.ac.in>
2026-06-17 22:13:18 +0530

    Refactored Code

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -171,35 +171,6 @@ function MovieList({ movies }) {
   )
 }
 
-/*
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
-
-
-
-        </>
-      )}
-    </div>
-  )
-}
-
-*/
-
 function Movie({ movie }) {
   return (
     <li key={movie.imdbID}>
